434c2b563a90a6f9d27697fda6337f1e7687472f
pnjun <fabiano.lever@gmail.com>
2018-04-25 23:47:24 +0200

    radio frames queue and callbacks

--- src/python/main.py
+++ src/python/main.py
@@ -1,26 +1,77 @@
 # -*- coding: utf-8 -*-
 #General Imports
 from radioHandler import RadioHandler
+from serial.serialutil import SerialException
+from queue import Queue
 
 #Kivy Imports
 from kivy.app import App
+from kivy.clock import Clock
 from kivy.lang import Builder
 from kivy.properties import ObjectProperty
+from kivymd.theming import ThemeManager
+from kivymd.tabs import MDTab
+
+class Launch(MDTab):
+    source = ObjectProperty(None)
 
 
 class InciWinchApp(App):
     theme_cls = ThemeManager()
-    previous_date = ObjectProperty()
     title = "InciWinch"
 
-    def build(self):
-        main_widget = Builder.load_file('gui.kv')
-        return main_widget
+    #FIFO queue for frames received over the radio that must be processed.
+    #The queue is filled by the radiomodule thread and emptied by the GUI thread
+    radioFrames = Queue()
 
+    def build(self):
+        return Builder.load_file('gui.kv')
+
+    def on_start(self):
+        try:
+            self.radio = RadioHandler("/dev/tty.usbserial-DN03FTBY", self.radioCallback)
+        except SerialException:
+            self.radio = None
+
+        #Radio frames processing is scheduled to run at every GUI loop cycle
+        #Increase kivy maxfps if needed (default is 60Hz)
+        Clock.schedule_interval(self.processFrames, 0)
+
+    def on_stop(self):
+        self.radio.halt()
+
+    def updateLaunch(self,launch, frame):
+        launch.ids.launchText.text = str(frame)
+
+    #This function is called from the xBee thread, do not call from GUI thread
+    def radioCallback(self, data):
+        self.radioFrames.put(data)
+
+    def processFrames(self, dt):
+        # This loop might freeze the UI if frames arrive faster
+        # than they can be processed. This could be solved by adding a
+        # max number of frames to processed per cicle, but that would not
+        # solve the problem of frames not beeing processed as soon as they arrrive.
+        # I want the app to crash if its overwhelmed, so that someone notices
+        # the problem instead of failing silently
+        while not self.radioFrames.empty():
+            frame = self.radioFrames.get(block=False)
+
+
+            #TODO: **************** FIX THIS SHIT **********************
+            tablist = self.root.ids.launchList.ids.tab_manager.screens
+
+            #Search if there is already a matching launch, if not create one
+            for launch in tablist:
+                if frame.source == launch.source:
+                    self.updateLaunch(launch, frame)
+                    break
+            else:
+                newLaunch = Launch()
+                newLaunch.source = frame.source
+                self.root.ids.launchList.add_widget(newLaunch)
+                self.updateLaunch(newLaunch, frame)
 
 
 if __name__ == '__main__':
     InciWinchApp().run()
-
-
-    radio = RadioHandler("/dev/tty.usbserial-DN03FTBY")
--- src/python/radioHandler.py
+++ src/python/radioHandler.py
@@ -1,8 +1,8 @@
 import serial
-import time
 import json
-from collections import namedtuple
-from tornado import ioloop, gen
+import binascii
+from datetime import datetime
+from types import SimpleNamespace
 from xbee import ZigBee
 
 BAUD_RATE = 9600
@@ -10,9 +10,24 @@ BAUD_RATE = 9600
 class RadioHandler:
     def __init__(self, serialPort, callback):
         self.port = serial.Serial(serialPort, BAUD_RATE)
-        xb = ZigBee(self.port, callback=self._recvCallback)
+        self.xbee = ZigBee(self.port, callback=self._recvCallback)
         self.callback = callback
 
     def _recvCallback(self, rawData):
-        data = json.loads(rawData, object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
-        self.callback(data)
+        source = rawData['source_addr_long']
+        payload = json.loads( rawData['rf_data'].decode('ascii') )
+
+        # This is here to explicitly decouple JSON naming convention
+        # from python internal variable names
+        data = {}
+        data['source'] = binascii.hexlify( source ).decode('ascii').upper()
+        data['time'] = datetime.now()
+        data['IAS'] = payload.pop('IAS')
+        data['height'] = payload.pop('HEI')
+
+        # Turns a dictionary into a namespace
+        self.callback(SimpleNamespace(**data))
+
+    def halt(self):
+        self.xbee.halt()
+        self.port.close()
