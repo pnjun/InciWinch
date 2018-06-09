76669b484b4850eaf53684abafab3d4d2276a0c2
pnjun <fabiano.lever@gmail.com>
2018-06-09 22:35:52 +0200

    schematics and PCB design

--- src/arduino/xBee/serial_relay/serial_relay.ino
+++ src/arduino/xBee/serial_relay/serial_relay.ino
@@ -1,28 +1,28 @@
 
 #include <SoftwareSerial.h>
 
-SoftwareSerial mySerial(2,3); // RX, TX
+SoftwareSerial mySerial(10,11); // RX, TX
 int incomingByte = 0;
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
-  Serial.begin(19200);
-  mySerial.begin(19200);
+  Serial.begin(9600);
+  mySerial.begin(9600);
 }
 
 // the loop function runs over and over again forever
 void loop() {
   if (mySerial.available() > 0)
   {
-     digitalWrite(LED_BUILTIN, HIGH);
-     delay(10);
-     digitalWrite(LED_BUILTIN, LOW);
-
      incomingByte = mySerial.read();
      Serial.write(incomingByte);
   }
   if (Serial.available() > 0)
   {
+     digitalWrite(LED_BUILTIN, HIGH);
+     delay(10);
+     digitalWrite(LED_BUILTIN, LOW);
+     
      incomingByte = Serial.read();
      mySerial.write(incomingByte);
   }
